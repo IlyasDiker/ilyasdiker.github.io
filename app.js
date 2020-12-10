bf34137b041267db8a8cdfcfa7ccb7753926b5dc
IlyasDiker <ilyasbenhssine@gmail.com>
2020-12-10 19:58:19 +0100

    add js

---
+++ app.js
@@ -0,0 +1,15 @@
+new Vue({
+    el: '#app',
+    bodyAttrs: {
+        class: "darken",
+        'vs-theme': 'dark'
+    },
+    data: {
+        APP_NAME : "Ilyas Benhssine Portfolio",
+        active: 1,
+        collapseOnScroll: true,
+    },
+    methods: {
+
+    }
+  })
