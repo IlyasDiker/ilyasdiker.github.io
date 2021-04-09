d53f2cec08861a141646c36c1972959962d104c0
IlyasDiker <ilyasbenhssine@gmail.com>
2021-04-09 18:40:12 +0100

    new portfolio wireframe

--- app.js
+++ app.js
@@ -1,18 +1,27 @@
+// -------------------------------------------------------------- //
+//                                       ::::::::::: :::::::::    //
+//                                          :+:     :+:    :+:    //
+//    By: Ilyas Benhssine                  +:+     +:+    +:+     //
+//                                        +#+     +#++:++#+       //
+//                                       +#+     +#+    +#+       //
+//    ilyasbenhssine@gmail.com          #+#     #+#    #+#        //
+//                                 ########### #########          //
+// -------------------------------------------------------------- //
+
+
 new Vue({
     el: '#app',
     bodyAttrs: {
-        class: "darken",
-        'vs-theme': 'dark'
+
     },
     data: {
-        APP_NAME : "Ilyas Benhssine Portfolio",
-        active: 1,
-        collapseOnScroll: true,
-        dialogactive: 0,
-        ddyslexia: false,
-        dcolorblind: false,
+        APP_NAME: "Ilyas Benhssine Portfolio",
+
     },
     methods: {
 
     }
-  })
+})
+
+
+//.show-onscroll
