9761b67c21d45a119593350a81ebfaf1bedfa159
IlyasDiker <ilyasbenhssine@gmail.com>
2021-04-09 22:13:17 +0100

    make navbar responsive

--- app.js
+++ app.js
@@ -28,9 +28,16 @@ new Vue({
 
 $('#nav-logo').hide();
 $('.show-onscroll').slideUp();
+responsive();
+
+$(window).resize(function() {
+    responsive();
+});
 
 
 $(window).on('scroll', function() {
+
+
     if($(window).scrollTop()){
         $('#nav-logo').show('slow');
         $('.show-onscroll').slideDown();
@@ -39,3 +46,21 @@ $(window).on('scroll', function() {
         $('.show-onscroll').slideUp();
     }
 });
+
+function closenav() {
+    $('#main-navbar').slideUp();
+}
+function opennav() {
+    $('#main-navbar').slideDown();
+}
+
+function responsive() {
+    if($(window).width() < 700) {
+        $('#main-navbar').slideUp('fast').addClass('responsive');
+        $('#responsive-btn').slideDown();
+    } else{
+        $('#main-navbar').removeClass('responsive');
+        $('#main-navbar').slideDown();
+        $('#responsive-btn').slideUp();
+    }
+}
