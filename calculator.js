c58e335ba28cefbcf7edb8348965e6ddc913bcac
IlyasDiker <ilyasbenhssine@gmail.com>
2021-05-07 14:07:33 +0000

    send old portfolio to trash

---
+++ calculator.js
@@ -0,0 +1,17 @@
+
+
+new Vue({
+    el: '#app',
+    bodyAttrs: {
+
+    },
+    data: {
+        APP_NAME: "Pricing Calculator",
+        PageNum: 0,
+        PageTest: 0,
+        total_price: this.PageNum,
+    },
+    methods: {
+
+    }
+})
